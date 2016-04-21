0d0f5a1241cff6d049c748193901e1196c4be032
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-04-20 19:37:10 -0700

    initial commit

---
+++ bids2isatab/main.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+#
+
+# import modules used here -- sys is a very standard one
+from __future__ import print_function
+import argparse
+import logging
+from collections import OrderedDict
+from glob import glob
+import os
+
+import nibabel
+import json
+import pandas as pd
+
+
+# Gather our code in a main() function
+from shutil import copy
+
+
+def get_metadata_for_nifti(bids_root, path):
+
+    sidecarJSON = path.replace(".nii.gz", ".json")
+
+    pathComponents = os.path.split(sidecarJSON)
+    filenameComponents = pathComponents[-1].split("_")
+    sessionLevelComponentList = []
+    subjectLevelComponentList = []
+    topLevelComponentList = []
+    ses = None;
+    sub = None;
+
+    for filenameComponent in filenameComponents:
+        if filenameComponent[:3] != "run":
+            sessionLevelComponentList.append(filenameComponent)
+            if filenameComponent[:3] == "ses":
+                ses = filenameComponent
+            else:
+                subjectLevelComponentList.append(filenameComponent)
+                if filenameComponent[:3] == "sub":
+                    sub = filenameComponent
+                else:
+                    topLevelComponentList.append(filenameComponent)
+
+    topLevelJSON = os.path.join(bids_root, "_".join(topLevelComponentList))
+    potentialJSONs = [topLevelJSON]
+
+    subjectLevelJSON = os.path.join(bids_root, sub, "_".join(subjectLevelComponentList))
+    potentialJSONs.append(subjectLevelJSON)
+
+    if ses:
+        sessionLevelJSON = os.path.join(bids_root, sub, ses, "_".join(sessionLevelComponentList))
+        potentialJSONs.append(sessionLevelJSON)
+
+    potentialJSONs.append(sidecarJSON)
+
+    merged_param_dict = {}
+    for json_file_path in potentialJSONs:
+        if os.path.exists(json_file_path):
+            param_dict = json.load(open(json_file_path, "r"))
+            merged_param_dict.update(param_dict)
+
+    return merged_param_dict
+
+
+def main(args, loglevel):
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
+
+    subject_ids = []
+    study_dict = OrderedDict()
+    for file in glob(os.path.join(args.bids_directory, "sub-*")):
+        if os.path.isdir(file):
+            subject_ids.append(os.path.split(file)[-1][4:])
+    subject_ids.sort()
+    study_dict["Source Name"] = subject_ids
+    study_dict["Characteristics[organism]"] = "Homo sapiens"
+    study_dict["Term Source REF"] = "NCBITAXON"
+    study_dict["Term Accession Number"] = "NCBITaxon:9606"
+    study_dict["Characteristics[organism part]"] = "brain"
+    study_dict["Protocol REF"] = "Participants"
+    study_dict["Sample Name"] = subject_ids
+    df = pd.DataFrame(study_dict)
+
+    participants_file = os.path.join(args.bids_directory, "participants.tsv")
+    if os.path.exists(participants_file):
+        participants_df = pd.read_csv(participants_file, sep="\t")
+        participants_df.rename(columns={'subject_id': "Sample Name"}, inplace=True)
+        participants_df["Sample Name"] = [s[4:] for s in list(participants_df["Sample Name"])]
+        for col in participants_df.columns.tolist():
+            if col != "Sample Name":
+                participants_df.rename(columns={col:"Comment[%s]"%col}, inplace=True)
+        df = pd.merge(df, participants_df, left_on="Sample Name", right_on="Sample Name")
+
+    df.to_csv(os.path.join(args.output_directory, "s_study.txt"), sep="\t", index=False)
+
+    assay_dict = OrderedDict()
+    sample_names = []
+    raw_file = []
+    types = []
+    resolutions = []
+    resolutions_units = []
+    rts = []
+    rts_units = []
+    other_fields = []
+    for file in glob(os.path.join(args.bids_directory, "sub-*", "*", "sub-*.nii.gz")) + \
+            glob(os.path.join(args.bids_directory, "sub-*", "ses-*", "*", "sub-*_ses-*.nii.gz")):
+        sample_names.append(os.path.split(file)[-1].split("_")[0][4:])
+        raw_file.append(file[len(args.bids_directory):])
+        types.append(file.split("_")[-1].split(".")[0])
+        header = nibabel.load(file).get_header()
+        resolutions.append("x".join([str(i) for i in header.get_zooms()[:3]]))
+        resolutions_units.append(header.get_xyzt_units()[0])
+        if len(header.get_zooms()) > 3:
+            rts.append(header.get_zooms()[3])
+            rts_units.append(header.get_xyzt_units()[1])
+        else:
+            rts.append(None)
+            rts_units.append(None)
+        other_fields.append(get_metadata_for_nifti(args.bids_directory, file))
+
+    assay_dict["Sample Name"] = sample_names
+    assay_dict["Raw Data File"] = raw_file
+    assay_dict["Type of MRI assay"] = types
+    assay_dict["Resolution value"] = resolutions
+    assay_dict["Resolution unit"] = resolutions_units
+
+    new_fields = set()
+    for d in other_fields:
+        new_fields = new_fields.union(set(d.keys()))
+
+    for field in new_fields:
+        assay_dict[field] = []
+        for d in other_fields:
+            if field in d:
+                assay_dict[field].append(d[field])
+            else:
+                assay_dict[field].append(None)
+
+    df = pd.DataFrame(assay_dict)
+    df.to_csv(os.path.join(args.output_directory, "a_assay.txt"), sep="\t", index=False)
+
+    this_path = os.path.join(os.path.realpath(__file__))
+    template_path = os.path.join(*(os.path.split(this_path)[:-1] + ("i_investigation_template.txt", )))
+    investigation_template = open(template_path).read()
+    with open(os.path.join(args.output_directory, "i_investigation.txt"), "w") as fp:
+        fp.write(investigation_template)
+
+
+
+
+
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Does a thing to some stuff.",
+        epilog="As an alternative to the commandline, params can be placed in a file, one per line, and specified on the"
+               " commandline like '%(prog)s @params.conf'.",
+        fromfile_prefix_chars='@')
+    # TODO Specify your real parameters here.
+    parser.add_argument(
+        "bids_directory",
+        help="Location of the root of your BIDS compatible directory",
+        metavar="BIDS_DIRECTORY")
+    parser.add_argument(
+        "output_directory",
+        help="Directory where ISA-TAB files will be stored",
+        metavar="OUTPUT_DIRECTORY")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="increase output verbosity",
+        action="store_true")
+    args = parser.parse_args()
+
+    # Setup logging
+    if args.verbose:
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.INFO
+
+    main(args, loglevel)
---
+++ setup.py
@@ -0,0 +1,67 @@
+from setuptools import setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name="BIDS2ISATab",
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='0.1.0',
+
+    description="Command line tool generating ISA-Tab compatible description from a Brain Imaging Data Structure "
+                "compatible dataset.",
+    long_description="Command line tool generating ISA-Tab compatible description from a Brain Imaging Data Structure "
+                     "compatible dataset.",
+
+    # The project URL.
+    url='https://github.com/INCF/BIDS2ISATab',
+
+    # Choose your license
+    license='BSD',
+
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='bids isatab',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages.
+    packages=["bids2isatab", "doc"],
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed.
+    install_requires = ["future",
+                        "pandas",
+                        'nibabel'],
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={},
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'bids2isatab=bids2isatab.main:main',
+        ],
+    },
+)
