1122ff9396ac979216c96ba125d83a96ff782c87
Michael Hanke <michael.hanke@gmail.com>
2016-08-03 15:59:40 +0200

    ENH: Be able to flatten hierarchical JSON
    This is helpful when more than the current BIDS set of information is
    available in the JSON structs (e.g. a DICOM dump). Nested "dicts" will
    appear as a flat list of columns with the key hierarchy presented as a
    concatenated string joined by ":'. For example:
    
    time -> samples -> TriggerTime
    
    yields
    
    Parameter Value[time:samples:TriggerTime]
    
    This change should have no effect on a plain BIDS metadata.

--- bids2isatab/main.py
+++ bids2isatab/main.py
@@ -63,6 +63,23 @@ def get_metadata_for_nifti(bids_root, path):
 
     return merged_param_dict
 
+def get_chainvalue(chain, src):
+    try:
+        for key in chain:
+            src = src[key]
+        return src
+    except KeyError:
+        return None
+
+def get_keychains(d, dest, prefix):
+    if isinstance(d, dict):
+        for item in d:
+            dest = get_keychains(d[item], dest, prefix + [item])
+    else:
+        if d and not (d == 'UNDEFINED'):
+            # ignore empty stuff
+            dest = dest.union((tuple(prefix),))
+    return dest
 
 def run(args, loglevel):
     logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
@@ -137,16 +154,14 @@ def run(args, loglevel):
 
     new_fields = set()
     for d in other_fields:
-        new_fields = new_fields.union(set(d.keys()))
+        new_fields = get_keychains(d, new_fields, [])
 
     for field in new_fields:
-        assay_dict["Parameter Value[%s]"%field] = []
+        column_id = "Parameter Value[%s]" % ':'.join(field)
+        assay_dict[column_id] = []
 
         for d in other_fields:
-            if field in d:
-                assay_dict["Parameter Value[%s]"%field].append(d[field])
-            else:
-                assay_dict["Parameter Value[%s]"%field].append(None)
+            assay_dict[column_id].append(get_chainvalue(field, d))
 
     assay_dict["Assay Name"] = assay_names
     assay_dict["Raw Data File"] = raw_file
